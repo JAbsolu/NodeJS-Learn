d4a1afc74b0e1b93bda0aa7cce6a5877f133f832
Johnson Absolu <Johnson.absolu@outlook.com>
2023-06-27 22:56:49 -0400

    update files

--- File System/02TUT/dir.js
+++ File System/02TUT/dir.js
@@ -1,10 +1,18 @@
 const fs = require('fs');
 
-if (!fs.existsSync('./new')) {
-    fs.mkdir('./new', (err) => {
+//Create a directory if it doesn't exist
+if (!fs.existsSync('./new')) { // If the folder new does not exist
+    fs.mkdir('./new', (err) => { //create the directory
         if (err) throw err;
         console.groupCollapsed(`Directory Created`);
     });
 };
 
+//Remove a directory if it exists
+if (fs.existsSync('./new')) { // If the folder new exists, delete the directory
+    fs.rmdir('./new', (err) => { //remove the directory
+        if (err) throw err;
+        console.groupCollapsed(`Directory removed`);
+    });
+};
 
