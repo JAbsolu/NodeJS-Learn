802a1470b3bf84e291758747850c2eb26f1c31fa
Johnson Absolu <Johnson.absolu@outlook.com>
2023-06-30 14:24:47 -0400

    update

---
+++ Web-Server/server.js
@@ -0,0 +1,93 @@
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+const fsPromises = require('fs').promisses;
+
+const logEvents = require('./logEvents');
+const EventEmitter = require('events');
+
+class Emitter extends EventEmitter {};
+const myEmitter = new Emitter();
+
+const PORT = process.env.PORT || 3500;
+
+const serveFile = async (filePath, contenType, response) => {
+    try {
+
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+const server = http.createServer((req, res) => {
+    console.log(req.url, req.method);
+
+    let current_path;
+
+    const extension = path.extname(req.url);
+
+    let contentType;
+
+    switch (extension) {
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.jpg':
+            contentType = 'image/jpeg';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;
+        case '.txt':
+            contentType = 'text/plain';
+            break;
+        default:
+            contentType = 'text/html';
+    }
+
+    let filePath = 
+        contentType === 'text/html' && req.url === '/'
+            ? path.join(__dirname, 'views', 'index.html')
+            : contentType === 'text/html' && req.url.slice(-1) === '/'
+                ? path.join(__dirname, 'views', req.url, 'index.html')
+                : contentType === 'text/html'
+                    ? path.join(__dirname, 'views', req.url)
+                    : path.join(__dirname, req.url);
+
+    // makes .html extension not required in the browser
+    if (!extension && req.url.slice(-1) !== '/') filePath += '.html';
+
+    const fileExists = fs.existsSync(filePath);
+
+    if (fileExists) {
+        //serve the file
+    } else {
+        switch(path.parse(filePath).base){
+            case 'old-page.html':
+                res.writeHead(301, {'Location' : '/new-page.html'});
+                res.end();
+                break;
+            case 'www-page.html': 
+                res.writeHead(301, {'Location' : '/'});
+                res.end();
+                break;
+            default:
+                // serve a 404 response
+
+        };
+    }
+});
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+
+
+
+
+// myEmitter.on('log', (msg) => logEvents(msg));
+// myEmitter.emit('log', 'Log event emitted!');
