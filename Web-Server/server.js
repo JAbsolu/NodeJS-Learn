d6a82800e2a9fafa38c02ddf7e0e717372219141
Johnson Absolu <Johnson.absolu@outlook.com>
2023-07-03 22:08:32 -0400

    web server lessons update

--- Web-Server/server.js
+++ Web-Server/server.js
@@ -1,28 +1,43 @@
 const http = require('http');
 const path = require('path');
 const fs = require('fs');
-const fsPromises = require('fs').promisses;
+const fsPromises = require('fs').promises;
 
 const logEvents = require('./logEvents');
 const EventEmitter = require('events');
-
 class Emitter extends EventEmitter {};
+// initialize object
 const myEmitter = new Emitter();
+myEmitter.on('log', (msg, fileName) => logEvents(msg, fileName ));
+const PORT = process.env.PORT || 3000;
 
-const PORT = process.env.PORT || 3500;
-
-const serveFile = async (filePath, contenType, response) => {
+const serveFile = async (filePath, contentType, response) => {
     try {
-
+        const rawData = await fsPromises.readFile(
+            filePath, 
+            !contentType.includes('image') ? 'utf8' : ''
+        );
+        const data = contentType === 'application/json'
+            ? JSON.parse(rawData) : rawData;
+        response.writeHead(
+            filePath.includes('404.html') ? 404 : 200, 
+            {'Content-Type' : contentType }
+        );
+        response.end(
+            contentType === 'application/json' ? JSON.stringify(data) : data
+        );
     } catch (err) {
         console.log(err);
+        myEmitter.emit('log', `${err.name}: ${err.message}`, 'errLog.txt');
+        response.statusCode = 500;
+        response.end();
     }
 }
 
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method);
+    myEmitter.emit('log', `${req.url}\t${req.method}`, 'reqLog.txt');
 
-    let current_path;
 
     const extension = path.extname(req.url);
 
@@ -66,7 +81,7 @@ const server = http.createServer((req, res) => {
     const fileExists = fs.existsSync(filePath);
 
     if (fileExists) {
-        //serve the file
+        serveFile(filePath, contentType, res);
     } else {
         switch(path.parse(filePath).base){
             case 'old-page.html':
@@ -78,16 +93,8 @@ const server = http.createServer((req, res) => {
                 res.end();
                 break;
             default:
-                // serve a 404 response
-
+                serveFile(path.join(__dirname, 'views', '404.html'), 'text/html', res);
         };
     }
 });
-
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
-
-
-
-
-// myEmitter.on('log', (msg) => logEvents(msg));
-// myEmitter.emit('log', 'Log event emitted!');
